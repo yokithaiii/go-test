5f58b6a4d3d38b86d1867dd278dd1b33c2eca696
aspartaue <spartakvasilev1999@gmail.com>
2024-04-21 18:33:18 +0900

    feat: add config

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	"github.com/yokithaiii/go-test.git"
 	"github.com/yokithaiii/go-test.git/pkg/handler"
 	"github.com/yokithaiii/go-test.git/pkg/repository"
@@ -10,12 +11,23 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error init config file. %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("cannot connect to server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	
+	return viper.ReadInConfig()
 }
